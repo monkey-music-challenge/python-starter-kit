769b942a1d66e4389b58dc5562a6f471353d480d
Oscar Söderlund <poscar@spotify.com>
2014-11-29 11:20:37 +0100

    Update to new API

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ import os
 import urllib
 
 # You control your monkey by sending POST requests to the Monkey Music server
-server_url = 'http://warmup.monkeymusicchallenge.com';
+GAME_URL = 'http://competition.monkeymusicchallenge.com/game';
 
 # Don't forget to provide the right command line arguments
-if len(sys.argv) < 3:
-    print('Usage: python index.py <your-team-name> <your-api-key>\n')
+if len(sys.argv) < 4:
+    print('Usage: python index.py <your-team-name> <your-api-key> <game-id>\n')
     if len(sys.argv) < 1:
         print('  Missing argument: <your-team-name>')
     if len(sys.argv) < 2:
         print('  Missing argument: <your-api-key>')
+    if len(sys.argv) < 3:
+        print('  Missing argument: <game-id>')
     sys.exit(1)
 
 # You identify yourselves by your team name
 team_name = sys.argv[1]
 api_key = sys.argv[2]
-
-# You POST to a team-specific URL:
-# warmup.monkeymusicchallenge.com/team/<your-team-name>
-# Surf to this URL and watch your monkey carry out your commands!
-team_url = '%s/team/%s' % (server_url, urllib.quote(team_name))
+game_id = sys.argv[3]
 
 # We've put the AI-code in a separate module
 import ai
@@ -33,56 +31,45 @@ def post_to_server(command):
     import json
     import requests
 
+    command['team'] = team_name
+    command['apiKey'] = api_key
+    command['gameId'] = game_id
+
+    print(command)
+    print(json.dumps(command))
+
     # Every time we POST a command to the server, we get a reply back
-    reply = requests.post(team_url,
+    reply = requests.post(GAME_URL,
                           data=json.dumps(command),
                           headers={'Content-Type': 'application/json'})
 
     # Hopefully, our server will always be able to handle your requests
     # but you never know...
     if reply.status_code != requests.codes.ok:
-        print('Error! We seem to have trouble talking to the server...\n')
         print('  The server replied with status code %d' % reply.status_code)
-
-        # The server specifies its own error messages on faulty requests
-        error = reply.json()
-        print('  %s' % error['message'])
+        try:
+            print('  %s' % reply.json()['message'])
+        except:
+            pass
         sys.exit(1)
 
     # The server replies with the current state of the game
     current_game_state = reply.json()
-
     return current_game_state
 
 # Allright, time to get started!
 
-# Make an initial post to start the game
-new_game_command = {
-    'command': 'new game',
-    'apiKey': api_key,
-}
-
-# The server replies with the initial game state
-current_game_state = post_to_server(new_game_command)
+# Send a join game command and the server replies with the initial game state
+current_game_state = post_to_server({'command': 'join game'})
 
 # The current game state tells you if you have any turns left
-while current_game_state['turns'] > 0:
-    print('Remaining turns: %d' % current_game_state['turns'])
+while not current_game_state['isGameOver']:
+    print('Remaining turns: %d' % current_game_state['remainingTurns'])
 
     # Use your AI to decide in which direction to move...
-    next_move_direction = ai.move(current_game_state)
-
-    # ...and send a new move command to the server
-    next_move_command = {
-        'command': 'move',
-        'direction': next_move_direction,
-        'apiKey': api_key,
-    }
+    next_command = ai.move(current_game_state)
 
     # After sending your next move, you'll get the new game state back
-    current_game_state = post_to_server(next_move_command)
+    current_game_state = post_to_server(next_command)
 
-# If the game is over, our server will tell you how you did
-# Go to http://warmup.monkeymusicchallenge.com/team/<your-team-name> for more details
 print('\nGame over.\n')
-print('  ' + current_game_state['message'])
